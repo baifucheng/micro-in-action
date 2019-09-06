6fca46fdc48ee5f87c69f8cac6793ee6a2bf186e
baifucheng <taynbfc@163.com>
2019-09-06 18:12:16 +0800

    redis serializer？

--- license-service/src/main/java/com/baifc/licenseservice/config/RedisConfig.java
+++ license-service/src/main/java/com/baifc/licenseservice/config/RedisConfig.java
@@ -2,6 +2,9 @@ package com.baifc.licenseservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 /**
  * projectName: micro-in-action
@@ -10,10 +13,17 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
  * Auther: baifc
  * Description:
  */
-//@Configuration
-//public class RedisConfig {
-//
-//    public RedisConnectionFactory redisConnectionFactory() {
-//
-//    }
-//}
+@Configuration
+public class RedisConfig {
+
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(factory);
+        // 这里需要使用定义序列化，默认的序列化存储到redis中，并不是原来指定的kv
+        GenericJackson2JsonRedisSerializer Jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        redisTemplate.setDefaultSerializer(Jackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
+}
